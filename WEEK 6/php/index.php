0f05eada075e755d9e63a3ff617d77a5630d3a17
Venu Agarwal <78161384+venu926@users.noreply.github.com>
2023-04-15 10:17:58 +0530

    Create index.php

---
+++ WEEK 6/php/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="CSS/style.css" type="text/css" media="screen" />
+    <meta charset="utf-8" />
+    <title>Index</title>
+    <script type= 'text/javascript' src='js/jquery-1.12.4.js'></script>
+    <script type= 'text/javascript' src='js/RotatingPictureDisplay.js'></script>
+  </head>
+  <body>
+    <div id = "bodyspecs">
+      <div id = "image-wrap">       
+        <img class="images"
+	     src="http://www.nskelectronics.in/image/cache/data/Category%20Images/Mini%20Project-200x200.png" 
+	     alt="MP">
+	<img class="images"
+             src="https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/062015/php_0.png?itok=ILNlhPBL" 
+       alt="PHP">
+       <img class="images"
+            src="http://www.princetonwebsystems.com/wp-content/uploads/2014/05/aws-logo-01.png" 
+      alt="AWS">
+      </div>
+      <header>
+        <nav class="center">
+          <ul id="list-nav">
+            <li><a href="index.php">Index</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="submit.php">Form</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div id = "bodyspecs2">
+        <h1> Hi, Welcome to my Mini Project #1</h1>
+        <section id = "classintro/repointro">
+          <h2> What's in this Repository? </h2>
+          <p> This repository is going to be used as the frontend for a Web Application for image storage and processing.</p>
+        </section>
+  
+        <footer id="contactinfo">
+          <address>
+	    <p>Get in touch with me:</p>
+	    <p>Personal Email: <a href="mailto:reachabdu@gmail.com">reachabdu@gmail.com</a></p>
+	    <p>Academic E-mail: <a href="mailto:asuhail@hawk.iit.edu">asuhail@hawk.iit.edu</a></p>
+	    <p>Visit me at my local library: 4 Friendship Plaza, Addison, IL 60101</p>
+	    <p>Coded by Abdullah Suhail (2017)</p>
+          </address>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
