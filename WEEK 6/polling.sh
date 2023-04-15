9bd299193a9c385d1ed0528a14557041e6110d69
Venu Agarwal <78161384+venu926@users.noreply.github.com>
2023-04-15 10:15:55 +0530

    Create polling.sh

---
+++ WEEK 6/polling.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Coded by: Abdullah Suhail
+#ITMO 544: Cloud Computing Technologies
+#Mini Project #2 polling (not really)
+
+
+sleep 90
+
+while true
+
+do
+php /var/www/html/asuhail_mp3/image-processing.php
+sleep 10
+
+done
